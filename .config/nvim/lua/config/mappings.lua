1e47ce9d263605279a361174d18cbf655823b18b
xxruyle <xrcnerd@gmail.com>
2025-03-28 11:29:35 -0500

    Latest

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -8,7 +8,7 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to up pane" })
 vim.keymap.set("n", "k", "gk", { desc = "Move up" })
 vim.keymap.set("n", "j", "gj", { desc = "Move down" })
 
--- file 
+-- file
 vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>qq", ":wqa<CR>", { desc = "Quit and save all open windows" })
 
@@ -16,26 +16,68 @@ vim.keymap.set("n", "<leader>qq", ":wqa<CR>", { desc = "Quit and save all open w
 vim.keymap.set("n", "<S-h>", ":BufferLineCyclePrev<CR>", { desc = "Move to next buffer" })
 vim.keymap.set("n", "<S-l>", ":BufferLineCycleNext<CR>", { desc = "Move to prev buffer" })
 
+-- line number
+vim.keymap.set("n", "<leader>sn", function()
+	vim.cmd("set number!")
+end, { desc = "Vim: set number (toggle line numbers showing)" })
 
--- line number 
-vim.keymap.set("n", "<leader>sn", function() vim.cmd("set number!") end, { desc = "Vim: set number (toggle line numbers showing)" })
-
--- line wrap 
-vim.keymap.set("n", "<leader>uw", function() vim.cmd("set wrap") end, { desc = "Vim: set wrap (toggle line wrap)" })
-
+-- line wrap
+vim.keymap.set("n", "<leader>uw", function()
+	vim.cmd("set wrap")
+end, { desc = "Vim: set wrap (toggle line wrap)" })
 
 -- clipboard
 vim.keymap.set("n", "<leader>y", "+y", { desc = "Yank to system clipboard" })
 vim.keymap.set("n", "<leader>y", "+y", { desc = "Yank to system clipboard" })
-vim.keymap.set("n", "<leader>yy","+yy", { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>yy", "+yy", { desc = "Yank to system clipboard" })
 
--- go back to last file 
+-- go back to last file
 -- vim.keymap.set("n", "<C-/>","<C-^>", { desc = "Go back to last file" })
 
--- center after ctrl u and ctrl d 
+-- center after ctrl u and ctrl d
 -- vim.keymap.set("n", "<C-u>","<C-u>zz", {desc="Move cursor up"})
 -- vim.keymap.set("n", "<C-d>","<C-d>zz", {desc="Move cursor up"})
 
 -- lsp-config
-vim.keymap.set("n", "<leader>lvt", function() vim.diagnostic.config({virtual_text=false}) end, { desc = "Toggles lsp inlay hints virtual text" })
-local diagnostics_active = true vim.keymap.set('n', '<leader>lt', function() diagnostics_active = not diagnostics_active if diagnostics_active then vim.diagnostic.enable(false) else vim.diagnostic.enable(true) end end, {desc="Toggle diagnostics"})
+vim.keymap.set("n", "<leader>lvt", function()
+	vim.diagnostic.config({ virtual_text = false })
+end, { desc = "Toggles lsp inlay hints virtual text" })
+local diagnostics_active = true
+vim.keymap.set("n", "<leader>lt", function()
+	diagnostics_active = not diagnostics_active
+	if diagnostics_active then
+		vim.diagnostic.enable(false)
+	else
+		vim.diagnostic.enable(true)
+	end
+end, { desc = "Toggle diagnostics" })
+
+-- lsp_signature diy
+vim.keymap.set("n", "<leader>fs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+
+vim.keymap.set("i", "<c-s>", function()
+	vim.lsp.buf.signature_help()
+end, { buffer = true })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers["signature_help"], {
+	border = "single",
+	close_events = { "CursorMoved", "BufHidden", "InsertCharPre" },
+})
+
+-- lsp signature as you type
+-- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers["signature_help"], {
+-- 	border = "single",
+-- 	close_events = { "CursorMoved", "BufHidden" },
+-- })
+-- vim.keymap.set("i", "<leader>fs", vim.lsp.buf.signature_help)
+
+-- Set up key mappings in your init.lua or any Lua configuration file
+
+-- Map 'gd' to go to the local definition (LSP)
+vim.keymap.set("n", "gd", function()
+	vim.lsp.buf.definition()
+end, { desc = "Go to definition" })
+
+-- Map 'gD' to go to the declaration (LSP)
+vim.keymap.set("n", "gD", function()
+	vim.lsp.buf.declaration()
+end, { desc = "Go to declaration" })
---
+++ .config/nvim/lua/plugins/typewrite.lua
@@ -0,0 +1,3 @@
+return {
+	{ "joshuadanpeterson/typewriter" },
+}
